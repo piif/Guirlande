3e8d7e306ff43102115431889d574cd9385c9085
piif <christian_lefebvre@laposte.net>
2019-11-17 16:28:59 +0100

    commit 2018 before rework for 2019

--- upload.sh
+++ upload.sh
@@ -1,1 +1,2 @@
-../arddude/etc/ad.sh -f avrdude -pm328p -carduino -P/dev/ttyACM0 -Uflash:w:Guirlande.hex.svg
+avrdude -pm328p -carduino -P/dev/ttyACM0 -Uflash:w:target/uno/Guirlande.hex
+#../arddude/etc/ad.sh -f avrdude -pm328p -carduino -P/dev/ttyACM0 -Uflash:w:Guirlande.hex.svg
